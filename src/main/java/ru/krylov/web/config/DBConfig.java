5c00a9d3e66b29e514767c60f8876adf425cecde
KrylovArtyom <88318874+KrylovArtyom@users.noreply.github.com>
2021-08-27 14:51:53 +0300

    complete

--- src/main/java/ru/krylov/web/config/DBConfig.java
+++ src/main/java/ru/krylov/web/config/DBConfig.java
@@ -17,7 +17,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:db.properties")
+@PropertySource ("classpath:db.properties")
 @EnableTransactionManagement
 public class DBConfig {
 
